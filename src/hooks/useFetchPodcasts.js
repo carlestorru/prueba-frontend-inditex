a8984f3d438d461400f41c21ab0a92d54ca752d9
Carles Torrubiano <carlestorru@gmail.com>
2024-04-27 12:57:36 +0200

    feat: custom hook fetches podcasts

---
+++ src/hooks/useFetchPodcasts.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import fetchAllOrigins from '../services/fetchAllOrigins';
+
+const URL =
+	'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json';
+
+export default function useFetchPodcasts() {
+	const [data, setData] = useState([]);
+	const [isFetching, setIsFetching] = useState(true);
+
+	useEffect(() => {
+		fetchAllOrigins(URL)
+			.then((res) => {
+				const parsedData = JSON.parse(res.contents);
+				const podcasts = parsedData.feed.entry;
+				setData(podcasts);
+			})
+			.catch((err) => console.log(err))
+			.finally(() => setIsFetching(false));
+	});
+
+	return [data, isFetching];
+}
