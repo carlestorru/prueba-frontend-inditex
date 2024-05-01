7e50ddd36380b008bd476c0a13793bc76ef00d11
Carles Torrubiano <carlestorru@gmail.com>
2024-05-01 10:07:45 +0200

    feat: podcast detail card layout

--- src/layouts/PodcastCardLayout.jsx
+++ src/layouts/PodcastCardLayout.jsx
@@ -1,11 +1,33 @@
 import { Outlet, useLocation } from 'react-router-dom';
-import PodcastCard from '../pages/PodcastDetails/PodcastCard';
+import Card from '../components/Card';
 
 export default function PodcastCardLayout() {
-	const { state } = useLocation();
+	const { state: data } = useLocation();
 	return (
-		<main className='flex flex-row justify-evenly p-4 gap-2'>
-			<PodcastCard data={state} />
+		<main className='flex flex-row max-sm:flex-col justify-evenly p-4 gap-2'>
+			<section>
+				<Card>
+					<div className='flex flex-col gap-2 p-2 w-[300px]'>
+						<div className='border-b-[1px] border-gray-400'>
+							<img
+								className='w-full p-1'
+								src={data['im:image'][2].label}
+								alt={data.id.attributes['im:id']}
+							/>
+						</div>
+						<div className='border-b-[1px] border-gray-400 p-2'>
+							<h3 className='font-bold text-xl'>{data['im:name'].label}</h3>
+							<h4 className='font-normal italic'>
+								by {data['im:artist'].label}
+							</h4>
+						</div>
+						<div>
+							<h5 className='font-bold text-wrap'>Description:</h5>
+							<p>{data.summary.label}</p>
+						</div>
+					</div>
+				</Card>
+			</section>
 
 			<Outlet />
 		</main>
