b48bdf747b5c57a08ca9d8a644c5993c72154b90
Carles Torrubiano <carlestorru@gmail.com>
2024-05-01 10:08:29 +0200

    feat: episode details view

--- src/pages/EpisodeDetails/index.jsx
+++ src/pages/EpisodeDetails/index.jsx
@@ -1,6 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
+import Card from '../../components/Card';
 
 export default function EpisodeDetails() {
-    const { episodeId } = useParams();
-    return <p>{episodeId}</p>
+	const { state } = useLocation();
+	console.log(state);
+	return (
+		<section>
+			<Card>
+				<div className='p-2'>
+					<h3 className='font-bold text-lg py-2'>{state['im:name'].label}</h3>
+					<p className='italic'>{state.summary.label}</p>
+                    <audio className='m-auto mt-8 py-2 w-full' src={state.episodeUrl} controls={true}></audio>
+				</div>
+			</Card>
+		</section>
+	);
 }
