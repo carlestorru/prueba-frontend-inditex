f045d1ba4604a5d3c43b6d844119d5c34c1f6fca
Carles Torrubiano <carlestorru@gmail.com>
2024-05-02 19:07:50 +0200

    test: header is rendered with link

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,30 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom'; // Si estás usando React Router
 import App from '../App';
+import MainPage from '../pages/MainPage';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { act } from 'react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const queryClient = new QueryClient();
+
+test('renders app with header component', async () => {
+	let container;
+
+	await act(async () => {
+		const { container: renderedContainer } = render(
+			<QueryClientProvider client={queryClient}>
+				<MemoryRouter initialEntries={['/']}>
+					<App />
+				</MemoryRouter>
+			</QueryClientProvider>,
+		);
+
+		container = renderedContainer;
+	});
+
+	const headerElement = container.querySelector('header');
+	expect(headerElement).toBeInTheDocument();
+
+	const linkElement = screen.getByRole('link', { name: 'Podcaster' });
+	expect(linkElement).toBeInTheDocument();
 });
