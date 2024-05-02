c36d565a218776d0fe05f6ef53837bef239d5023
Carles Torrubiano <carlestorru@gmail.com>
2024-05-02 20:32:19 +0200

    style: add hover effect

--- src/pages/MainPage/components/PodcastCard.jsx
+++ src/pages/MainPage/components/PodcastCard.jsx
@@ -2,7 +2,7 @@ import Card from '../../../components/Card';
 
 export default function PodcastCard({ data }) {
 	return (
-		<div className='flex flex-col items-center text-center m-auto'>
+		<div className='flex flex-col items-center text-center m-auto hover:scale-110 transition duration-300'>
 			<img
 				className='rounded-full -mb-8 z-10 h-20 w-20'
 				src={data['im:image'][0].label}
