d66d94ebeffa5e4cf6633671caec6dc99862b55c
Carles Torrubiano <carlestorru@gmail.com>
2024-05-02 17:40:48 +0200

    feat: add img and title link to podcast detail page

--- src/layouts/PodcastCardLayout.jsx
+++ src/layouts/PodcastCardLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 import Card from '../components/Card';
 
 export default function PodcastCardLayout() {
@@ -9,17 +9,25 @@ export default function PodcastCardLayout() {
 				<Card>
 					<div className='flex flex-col gap-2 p-2 w-[300px]'>
 						<div className='border-b-[1px] border-gray-400'>
-							<img
-								className='w-full p-1'
-								src={data['im:image'][2].label}
-								alt={data.id.attributes['im:id']}
-							/>
+							<Link
+								to={`/podcast/${data.id.attributes['im:id']}`}
+								state={{ ...data }}>
+								<img
+									className='w-full p-1'
+									src={data['im:image'][2].label}
+									alt={data.id.attributes['im:id']}
+								/>
+							</Link>
 						</div>
 						<div className='border-b-[1px] border-gray-400 p-2'>
-							<h3 className='font-bold text-xl'>{data['im:name'].label}</h3>
-							<h4 className='font-normal italic'>
-								by {data['im:artist'].label}
-							</h4>
+							<Link
+								to={`/podcast/${data.id.attributes['im:id']}`}
+								state={{ ...data }}>
+								<h3 className='font-bold text-xl'>{data['im:name'].label}</h3>
+								<h4 className='font-normal italic'>
+									by {data['im:artist'].label}
+								</h4>
+							</Link>
 						</div>
 						<div>
 							<h5 className='font-bold text-wrap'>Description:</h5>
