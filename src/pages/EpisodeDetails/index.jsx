4e265618961745c259a7cf1e1120d5f5aa0e8a0c
Carles Torrubiano <carlestorru@gmail.com>
2024-05-02 18:35:25 +0200

    remove console.log

--- src/pages/EpisodeDetails/index.jsx
+++ src/pages/EpisodeDetails/index.jsx
@@ -3,14 +3,20 @@ import Card from '../../components/Card';
 
 export default function EpisodeDetails() {
 	const { state } = useLocation();
-	console.log(state);
+
 	return (
 		<section>
 			<Card>
 				<div className='p-2'>
 					<h3 className='font-bold text-lg py-2'>{state.trackName}</h3>
-					<div className='italic' dangerouslySetInnerHTML={{ __html: state.description }} />
-                    <audio className='m-auto mt-8 py-2 w-full' src={state.episodeUrl} controls={true}></audio>
+					<div
+						className='italic'
+						dangerouslySetInnerHTML={{ __html: state.description }}
+					/>
+					<audio
+						className='m-auto mt-8 py-2 w-full'
+						src={state.episodeUrl}
+						controls={true}></audio>
 				</div>
 			</Card>
 		</section>
