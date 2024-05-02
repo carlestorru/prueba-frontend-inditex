368e969dfebcddb1b33521bc2109e00b1c836843
Carles Torrubiano <carlestorru@gmail.com>
2024-05-02 18:08:49 +0200

    fix: html header tag

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
 	return (
-		<div className="font-bold text-sky-700 text-xl p-2 border-b-[1px] border-gray-400">
+		<header className="font-bold text-sky-700 text-xl p-2 border-b-[1px] border-gray-400">
             <Link to='/'>
 			<h1>Podcaster</h1>
             </Link>
-		</div>
+		</header>
 	);
 }
